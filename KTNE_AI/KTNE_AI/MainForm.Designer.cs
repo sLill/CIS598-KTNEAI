67e1352296b2aa4dd77431bea7e6a04912752686
sLill <ssl@ksu.edu>
2017-12-15 12:54:05 -0600

    -

--- KTNE_AI/KTNE_AI/MainForm.Designer.cs
+++ KTNE_AI/KTNE_AI/MainForm.Designer.cs
@@ -31,14 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
             this.rtbLog = new System.Windows.Forms.RichTextBox();
             this.btnBegin = new System.Windows.Forms.Button();
-            this.waveViewer = new NAudio.Gui.WaveViewer();
             this.SuspendLayout();
             // 
             // rtbLog
             // 
             this.rtbLog.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.rtbLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbLog.Location = new System.Drawing.Point(12, 130);
+            this.rtbLog.Location = new System.Drawing.Point(13, 12);
             this.rtbLog.Name = "rtbLog";
             this.rtbLog.ReadOnly = true;
             this.rtbLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
@@ -50,7 +49,7 @@
             // 
             this.btnBegin.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.btnBegin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBegin.Location = new System.Drawing.Point(12, 326);
+            this.btnBegin.Location = new System.Drawing.Point(12, 218);
             this.btnBegin.Name = "btnBegin";
             this.btnBegin.Size = new System.Drawing.Size(75, 23);
             this.btnBegin.TabIndex = 1;
@@ -58,25 +57,13 @@
             this.btnBegin.UseVisualStyleBackColor = false;
             this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
             // 
-            // waveViewer
-            // 
-            this.waveViewer.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.waveViewer.Location = new System.Drawing.Point(12, 12);
-            this.waveViewer.Name = "waveViewer";
-            this.waveViewer.SamplesPerPixel = 128;
-            this.waveViewer.Size = new System.Drawing.Size(623, 74);
-            this.waveViewer.StartPosition = ((long)(0));
-            this.waveViewer.TabIndex = 2;
-            this.waveViewer.WaveStream = null;
-            // 
             // mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(648, 361);
-            this.Controls.Add(this.waveViewer);
+            this.ClientSize = new System.Drawing.Size(648, 258);
             this.Controls.Add(this.btnBegin);
             this.Controls.Add(this.rtbLog);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -90,7 +77,6 @@
 
         private System.Windows.Forms.RichTextBox rtbLog;
         private System.Windows.Forms.Button btnBegin;
-        private NAudio.Gui.WaveViewer waveViewer;
     }
 }
 
